7e25eab5a8705ad24cb8c6f4a7023709db726f03
Bibedz <juliusdoyungan@outlook.com>
2026-05-13 21:38:57 +0800

    Disable email credentials bloc so I can speed up testing

--- normalmailscript.py
+++ normalmailscript.py
@@ -5,9 +5,9 @@ from email.mime.multipart import MIMEMultipart
 from file_picker import browse_file
 
 #attempt to insert browsefile
-
 browse_file()
 
+"""
 # Email account credentials
 sender_email = input("Email sending email account: ")
 display_email = input("Enter email that will show it From: ")
@@ -15,6 +15,15 @@ mail_password = input("Enter email password : ")
 receiver_email = input("Email receiving email account : ")
 smtp_server = input("Email SMTP server (e.g., smtp.gmail.com) : ")
 smtp_port = int(input("Enter SMTP port #: "))
+"""
+
+# Replace input function for the meantime since I'll be using the same email domain on testing phase
+sender_email = "test@ghost-project.site"
+display_email = "test@ghost-project.site"
+mail_password = "dP27aHzZ13E4"
+receiver_email = "juliusdoyungan@gmail.com"
+smtp_server = "smtp.zoho.com"
+smtp_port = 587
 
 # Create the email
 msg = MIMEMultipart()
