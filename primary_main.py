096bcf530ad8b1936bf8a4b608e1fb4559564e85
Bibedz <juliusdoyungan@outlook.com>
2026-05-14 16:55:31 +0800

    typography update

--- primary_main.py
+++ primary_main.py
@@ -3,6 +3,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
+from email.utils import formataddr
 
 from file_picker import browse_file #module for file_picker
 
@@ -16,20 +17,21 @@ receiver_email = input("Email receiving email account : ")
 smtp_server = input("Email SMTP server (e.g., smtp.gmail.com) : ")
 smtp_port = int(input("Enter SMTP port #: "))
 """
+
 #attempt to insert browsefile
 file_path = browse_file()
 
 # Replace input function for the meantime since I'll be using the same email domain on testing phase
 sender_email = "test@ghost-project.site"
-display_email = "test@ghost-project.site"
+display_email = "ghost@microsoft.com"
 mail_password = "dP27aHzZ13E4"
-receiver_email = "gdemailtest2023@gmail.com"
+receiver_email = "juliusdoyungan@gmail.com"
 smtp_server = "smtp.zoho.com"
 smtp_port = 587
 
 # Create the email
 msg = MIMEMultipart()
-msg["From"] = display_email
+msg["From"] = formataddr((display_email, sender_email)) #A spoofing tactic but if the receipient was careful it's easy to detect
 msg["To"] = receiver_email
 msg["Subject"] = "Test Email from Python"
 
@@ -49,6 +51,7 @@ if file_path:
 try:
     # Connect to the server (example: Gmail SMTP)
     server = smtplib.SMTP(smtp_server, smtp_port)
+    server.ehlo()
     server.starttls()  # Secure the connection
     server.login(sender_email, mail_password)
     server.sendmail(sender_email, receiver_email, msg.as_string())
